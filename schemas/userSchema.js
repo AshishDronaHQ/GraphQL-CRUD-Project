cdd375b271de1247d4e9a114a46916acbfe6ce70
AshishDronaHQ <161713380+AshishDronaHQ@users.noreply.github.com>
2024-03-16 00:19:59 +0530

    test

---
+++ schemas/userSchema.js
@@ -0,0 +1,24 @@
+// schemas/userSchema.js
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    id: ID!
+    name: String!
+    email: String!
+    age: Int!
+  }
+
+  type Query {
+    users: [User!]!
+    user(id: ID!): User
+  }
+
+  type Mutation {
+    createUser(name: String!, email: String!, age: Int!): User!
+    updateUser(id: ID!, name: String, email: String, age: Int): User!
+    deleteUser(id: ID!): User!
+  }
+`;
+
+module.exports = typeDefs;
